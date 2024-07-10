45890866c5ffb8a833fe1aa2cf76d34b50ecef26
Reyyy27 <kakreihan27@gmail.com>
2024-07-10 13:56:32 +0800

    membuat seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->withPersonalTeam()->create();
-
-        User::factory()->withPersonalTeam()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UsersTableSeeder::class,
+            DetailUserTableSeeder::class,
+            OrderStatusTableSeeder::class,
         ]);
     }
 }
